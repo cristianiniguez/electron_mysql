4a250f301f708404c9b8a3c02c7de37e6410b004
cristianiniguez <felizpc1@gmail.com>
2020-09-20 16:13:10 -0400

    CRUD functions maded

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 const { BrowserWindow, Notification } = require('electron')
 const { getConnection } = require('./database')
 
+async function getProducts() {
+  try {
+    const cn = await getConnection()
+    const results = await cn.query('SELECT * FROM products ORDER BY id DESC')
+    return results
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function deleteProduct(id) {
+  try {
+    const cn = await getConnection()
+    const result = await cn.query('DELETE FROM products WHERE id = ?', id)
+    return result
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 async function createProduct(product) {
   try {
     const cn = await getConnection()
@@ -12,7 +32,27 @@ async function createProduct(product) {
     product.id = result.insertId
     return product
   } catch (error) {
-    console.log(error)
+    console.error(error)
+  }
+}
+
+async function getProductById(id) {
+  try {
+    const cn = await getConnection()
+    const results = await cn.query('SELECT * FROM products WHERE id = ?', id)
+    return results[0]
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function updateProduct(id, product) {
+  try {
+    const cn = await getConnection()
+    const result = await cn.query('UPDATE products SET ? WHERE id = ?', [product, id])
+    return result
+  } catch (error) {
+    console.error(error)
   }
 }
 
@@ -32,5 +72,9 @@ function createWindow() {
 
 module.exports = {
   createWindow,
-  createProduct
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct
 }
--- src/ui/app.js
+++ src/ui/app.js
@@ -1,9 +1,55 @@
 const { remote } = require('electron')
-console.log(remote)
 const main = remote.require('./main')
+
+// Variables
+let products = []
+let editingStatus = false
+let productId = ''
 
 // Elements
 const $productForm = document.getElementById('product-form')
+const $productsList = document.getElementById('products')
+
+// Functions
+const getProducts = async () => {
+  products = await main.getProducts()
+  renderProducts(products)
+}
+
+const editProduct = async (id) => {
+  const product = await main.getProductById(id)
+  $productForm['name'].value = product.name
+  $productForm['price'].value = product.price
+  $productForm['description'].value = product.description
+  editingStatus = true
+  productId = product.id
+}
+
+const deleteProduct = async (id) => {
+  const response = confirm('Are you sure you want to delete it?')
+  if (response) {
+    const result = await main.deleteProduct(id)
+    console.log(result)
+    getProducts()
+  }
+}
+
+const renderProducts = (products) => {
+  $productsList.innerHTML = ""
+  products.forEach(product => {
+    $productsList.innerHTML += `
+      <div class="card card-body my-2 animate__animated animate__fadeInLeft">
+        <h4>${product.name}</h4>
+        <p>${product.description}</p>
+        <h3>${product.price}</h3>
+        <p>
+          <button class="btn btn-danger" onclick="deleteProduct(${product.id})">DELETE</button>
+          <button class="btn btn-secondary" onclick="editProduct(${product.id})">EDIT</button>
+        </p>
+      </div>
+    `
+  })
+}
 
 // Events
 $productForm.addEventListener('submit', async e => {
@@ -15,6 +61,21 @@ $productForm.addEventListener('submit', async e => {
     description: $productForm['description'].value
   }
 
-  const result = await main.createProduct(newProduct)
-  console.log(result)
+  if (!editingStatus) {
+    const result = await main.createProduct(newProduct)
+    console.log(result)
+  } else {
+    const result = await main.updateProduct(productId, newProduct)
+    console.log(result)
+    editingStatus = false
+    productId = ""
+  }
+
+  $productForm.reset()
+  $productForm['name'].focus()
+  getProducts()
+})
+
+window.addEventListener('load', async () => {
+  await getProducts()
 })
